2afa53856ceaf4dc48274252cab72b1e839bf826
Erick Martinez <149088745+ErickMartinezNeduga@users.noreply.github.com>
2024-05-06 11:22:14 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig, ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/tarjetadigital/',
-  plugins: [react()],
-  build: {
-    outDir: 'tarjetadigital', // Directorio de salida para los archivos de producción
-    assetsDir: 'assets', // Carpeta donde se almacenarán los assets
-    rollupOptions: {
-      output: {
-        
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
+// Definir un tipo para el parámetro de la función de configuración
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  // Cargar todas las variables de entorno
+  const env = loadEnv(mode, process.cwd())
+
+  // Determinar el valor base dependiendo del entorno
+  const base = env.VITE_APP_ENV === 'Development' ? '/tarjetadigital_prueba/' : '/tarjetadigital/';
+  const outDir =  env.VITE_APP_ENV === 'Development' ? 'tarjetadigital_prueba' : 'tarjetadigital';
+
+  return {
+    base: base,
+    plugins: [react()],
+    build: {
+      outDir: outDir, // Directorio de salida para los archivos de producción
+      assetsDir: 'assets', // Carpeta donde se almacenarán los assets
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/[name]-[hash].js',
+          entryFileNames: 'assets/[name]-[hash].js'
+        }
       }
-
     }
-  }
-})
+  };
+});
